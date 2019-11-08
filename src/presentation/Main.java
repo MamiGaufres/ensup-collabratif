6244015dc70ba1ad182728c19434cd7fa2a9da98
MamiGaufres <mogofranca@live.fr>
2019-11-08 11:24:31 +0100

    heheheh

--- src/presentation/Main.java
+++ src/presentation/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// TEST Morgan
-		// TEST Braham je suis al les gars !
-		// TEST Lyes et du coup apres ca dis quoi?
+		
+		// QUUIII SUCE ?
 		
 
 	}
